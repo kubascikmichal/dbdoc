8814bf0f7d178f5d6040236cc125799a00fdee17
Michal Kubaščík <michal.kubascik26799@gmail.com>
2024-03-04 16:59:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "html", "index.html"));
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
